90234dbbe62e1305f3e04f9d502115fd27033317
Toshiaki Maki <tmaki@pivotal.io>
2018-01-17 20:00:01 +0900

    Add more hints

--- src/main/java/com/example/blog/webhook/EntryFetcher.java
+++ src/main/java/com/example/blog/webhook/EntryFetcher.java
@@ -5,7 +5,9 @@ import java.nio.file.Paths;
 import am.ik.blog.entry.Entry;
 import am.ik.blog.entry.EntryId;
 import am.ik.github.GitHubClient;
+import am.ik.github.repositories.commits.CommitsResponse.Commit;
 import am.ik.github.repositories.contents.ContentsResponse.File;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class EntryFetcher {
@@ -18,6 +20,8 @@ public class EntryFetcher {
 	public Mono<Entry> fetch(String owner, String repo, String path) {
 		EntryId entryId = EntryId.fromFilePath(Paths.get(path));
 		Mono<File> file = this.gitHubClient.file(owner, repo, path).get();
+		Flux<Commit> commits = this.gitHubClient.commits(owner, repo)
+				.get(params -> params.path(path));
 
 		// TODO: 実装してください
 		return Mono.empty();
